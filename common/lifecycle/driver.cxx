db5b409f5e6e5c5d3771febd8046240cdb3e06e4
Boris Kolpackov <boris@codesynthesis.com>
2011-01-04 16:57:28 +0200

    Copyright update

--- common/lifecycle/driver.cxx
+++ common/lifecycle/driver.cxx
@@ -1,6 +1,6 @@
 // file      : common/lifecycle/driver.cxx
 // author    : Boris Kolpackov <boris@codesynthesis.com>
-// copyright : Copyright (c) 2009-2010 Code Synthesis Tools CC
+// copyright : Copyright (c) 2009-2011 Code Synthesis Tools CC
 // license   : GNU GPL v2; see accompanying LICENSE file
 
 // Test object state transistions.
