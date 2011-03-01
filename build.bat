76108c5211713934c20fdd8a78755e5b21697e1b
Boris Kolpackov <boris@codesynthesis.com>
2011-03-01 16:13:25 +0200

    Add support for building only certain configurations/platforms on Windows

--- build.bat
+++ build.bat
@@ -5,16 +5,15 @@ rem copyright : Copyright (c) 2009-2011 Code Synthesis Tools CC
 rem license   : GNU GPL v2; see accompanying LICENSE file
 
 rem
-rem build.bat database vc-version [/Build|/Clean|/Rebuild]
-rem
 rem Build tests using the VC++ batch mode compilation.
+rem
+rem build.bat database vc-version conf plat [/Build|/Clean|/Rebuild]
+rem
+rem conf: {Debug,Release}|all
+rem plat: {Win32,x64}|all
 rem
 
 setlocal
-
-set "confs=__path__(configurations)"
-set "plats=__path__(platforms)"
-set "failed="
 
 if "_%1_" == "__" (
   echo no database specified
@@ -26,6 +25,18 @@ if "_%2_" == "__" (
   goto usage
 )
 
+if "_%~3_" == "__" (
+  echo no configuration specified
+  goto usage
+)
+
+if "_%~4_" == "__" (
+  echo no platform specified
+  goto usage
+)
+
+set "failed="
+
 if "_%2_" == "_9_" set "vcver=9"
 if "_%2_" == "_10_" set "vcver=10"
 
@@ -34,7 +45,13 @@ if "_%vcver%_" == "__" (
   goto usage
 )
 
-set "action=%3"
+set "confs=%~3"
+set "plats=%~4"
+
+if "_%confs%_" == "_all_" set "confs=__path__(configurations)"
+if "_%plats%_" == "_all_" set "plats=__path__(platforms)"
+
+set "action=%5"
 if "_%action%_" == "__" set "action=/Build"
 
 set "devenv=%DEVENV%"
@@ -85,8 +102,10 @@ goto end
 
 :usage
 echo.
-echo usage: build.bat database vc-version [action]
-echo   valid actions are /Build, /Clean, and /Rebuild
+echo usage: build.bat database vc-version conf plat [action]
+echo   valid configurations are: {Debug,Release}|all
+echo   valid platforms are: {Win32,x64}|all
+echo   valid actions are: /Build (default), /Clean, and /Rebuild
 echo.
 
 :error
