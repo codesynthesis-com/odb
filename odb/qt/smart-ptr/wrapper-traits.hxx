fc0acda2f0a9f27b490df84de2580306942e42ea
Boris Kolpackov <boris@codesynthesis.com>
2011-08-04 13:33:21 +0200

    Add support for Qt QSharedPointer as value wrapper

---
+++ odb/qt/smart-ptr/wrapper-traits.hxx
@@ -0,0 +1,60 @@
+// file      : odb/qt/smart-ptr/wrapper-traits.hxx
+// author    : Constantin Michael <constantin@codesynthesis.com>
+// copyright : Copyright (c) 2009-2011 Code Synthesis Tools CC
+// license   : GNU GPL v2; see accompanying LICENSE file
+
+#ifndef ODB_QT_SMART_PTR_WRAPPER_TRAITS_HXX
+#define ODB_QT_SMART_PTR_WRAPPER_TRAITS_HXX
+
+#include <odb/pre.hxx>
+
+#include <QtCore/QSharedPointer>
+
+#include <odb/wrapper-traits.hxx>
+
+namespace odb
+{
+  // Specialization for QSharedPointer.
+  //
+  template <typename T>
+  class wrapper_traits< QSharedPointer<T> >
+  {
+  public:
+    typedef T wrapped_type;
+    typedef QSharedPointer<T> wrapper_type;
+
+    static const bool null_handler = true;
+    static const bool null_default = false;
+
+    static bool
+    get_null (const wrapper_type& p)
+    {
+      return p.isNull ();
+    }
+
+    static void
+    set_null (wrapper_type& p)
+    {
+      p.clear ();
+    }
+
+    static const wrapped_type&
+    get_ref (const wrapper_type& p)
+    {
+      return *p;
+    }
+
+    static wrapped_type&
+    set_ref (wrapper_type& p)
+    {
+      if (p.isNull ())
+        p = wrapper_type (new wrapped_type);
+
+      return *p;
+    }
+  };
+}
+
+#include <odb/post.hxx>
+
+#endif // ODB_QT_SMART_PTR_WRAPPER_TRAITS_HXX
