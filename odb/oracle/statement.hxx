37e0c8b73068fc3c16201502c5b8dbee2b8e7605
Constantin Michael <constantin@codesynthesis.com>
2011-09-08 08:54:28 +0200

    Update comments

--- odb/oracle/statement.hxx
+++ odb/oracle/statement.hxx
@@ -36,16 +36,16 @@ namespace odb
     protected:
       statement (connection&, const std::string& statement);
 
-      // Bind output parameters to this statement. This function must
-      // only be called once. Multiple calls to it will result in memory leaks
-      // due to lost OCIBind resources.
+      // Bind output parameters to this statement. This function must only
+      // be called once. Multiple calls to it will result in memory leaks due
+      // to lost OCIBind resources.
       //
       void
       bind_param (bind*, std::size_t count, std::size_t offset);
 
-      // Bind input parameters to this statement. This function must
-      // only be called once. Multiple calls to it will result in memory leaks
-      // due to lost OCIDefine resources.
+      // Bind input parameters to this statement. This function must only be
+      // called once. Multiple calls to it will result in memory leaks due to
+      // lost OCIDefine resources.
       //
       void
       bind_result (bind*, std::size_t count);
@@ -108,8 +108,8 @@ namespace odb
                         binding& data,
                         bool returning);
 
-      // Return true if successful and false if the row is a duplicate.
-      // All other errors are reported by throwing exceptions.
+      // Return true if successful and false if the row is a duplicate. All
+      // other errors are reported by throwing exceptions.
       //
       bool
       execute ();
@@ -121,6 +121,12 @@ namespace odb
       insert_statement (const insert_statement&);
       insert_statement& operator= (const insert_statement&);
 
+      // Only OCI versions 11.2 and greater support conversion of the internal
+      // Oracle type NUMBER to an external 64-bit integer type. If we detect
+      // version 11.2 or greater, we leverage this feature and provide an
+      // unsigned long long image. Otherwise, we revert to using a 32-bit
+      // unsigned integer.
+      //
     public:
       struct id_bind_type
       {
