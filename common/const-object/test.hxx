e39f1cf9f0c3f520c61e0b832cca78df715d658d
Boris Kolpackov <boris@codesynthesis.com>
2011-10-19 10:58:44 +0200

    Rename common/const test to common/const-object

--- common/const-object/test.hxx
+++ common/const-object/test.hxx
@@ -1,4 +1,4 @@
-// file      : common/const/test.hxx
+// file      : common/const-object/test.hxx
 // author    : Boris Kolpackov <boris@codesynthesis.com>
 // copyright : Copyright (c) 2009-2011 Code Synthesis Tools CC
 // license   : GNU GPL v2; see accompanying LICENSE file
