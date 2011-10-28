729c51710404d9866c92d66928b69fc89070d457
Boris Kolpackov <boris@codesynthesis.com>
2011-10-28 10:27:30 +0200

    Introduce interface version for Qt and Boost profiles
    This will allow us to make bugfix releases without having to update
    ODB compiler binaries, which include these libraries.

--- odb/qt/version.hxx
+++ odb/qt/version.hxx
@@ -33,8 +33,16 @@
 #  error incompatible odb interface version detected
 #endif
 
-// libodb-qt version: odb interface version plus the bugfix
+// ODB Qt interface version: odb interface version plus the Qt interface
 // version.
+//
+#define ODB_QT_VERSION     1060000
+#define ODB_QT_VERSION_STR "1.6.0"
+
+// libodb-qt version: odb interface version plus the bugfix version. Note
+// that LIBODB_QT_VERSION is always greater or equal to ODB_QT_VERSION
+// since if the Qt interface virsion is incremented then the bugfix version
+// must be incremented as well.
 //
 #define LIBODB_QT_VERSION     1060000
 #define LIBODB_QT_VERSION_STR "1.6.0"
