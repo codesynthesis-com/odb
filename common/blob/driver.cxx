1bbe6fe0d6269851ac69a383189b0809d86ef40f
Boris Kolpackov <boris@codesynthesis.com>
2011-11-03 10:11:24 +0200

    Add support for mapping char[N] and unsigned char[N] types to BLOB
    New test: common/blob.

---
+++ common/blob/driver.cxx
@@ -0,0 +1,71 @@
+// file      : common/blob/driver.cxx
+// author    : Boris Kolpackov <boris@codesynthesis.com>
+// copyright : Copyright (c) 2009-2011 Code Synthesis Tools CC
+// license   : GNU GPL v2; see accompanying LICENSE file
+
+// Test BLOB mapping.
+//
+
+#include <memory>   // std::auto_ptr
+#include <cassert>
+#include <iostream>
+
+#include <odb/database.hxx>
+#include <odb/transaction.hxx>
+
+#include <common/common.hxx>
+
+#include "test.hxx"
+#include "test-odb.hxx"
+
+using namespace std;
+using namespace odb::core;
+
+int
+main (int argc, char* argv[])
+{
+  try
+  {
+    auto_ptr<database> db (create_database (argc, argv));
+
+    const char data[] =
+      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+      "\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0A\x0B"
+      "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb"
+      "\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0A\x0B"
+      "cccccccccccccccccccccccccccccccccccccccccccccccc"
+      "\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0A\x0B"
+      "dddddddddddddddddddddddddddddddddddddddddddddddd"
+      "\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0A\x0B"
+      "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"
+      "\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0A\x0B"
+      "ffffffffffffffffffffffffffffffffffffffffffffffff";
+
+    const unsigned char* udata = reinterpret_cast<const unsigned char*> (data);
+
+    object o (1);
+    o.vc.assign (data, data + sizeof (data));
+    o.vuc.assign (udata, udata + sizeof (data));
+    memcpy (o.c, data, sizeof (data));
+    memcpy (o.uc, udata, sizeof (data));
+
+    {
+      transaction t (db->begin ());
+      db->persist (o);
+      t.commit ();
+    }
+
+    {
+      transaction t (db->begin ());
+      auto_ptr<object> o1 (db->load<object> (1));
+      t.commit ();
+
+      assert (o == *o1);
+    }
+  }
+  catch (const odb::exception& e)
+  {
+    cerr << e.what () << endl;
+    return 1;
+  }
+}
---
+++ common/blob/test.hxx
@@ -0,0 +1,55 @@
+// file      : common/blob/test.hxx
+// author    : Boris Kolpackov <boris@codesynthesis.com>
+// copyright : Copyright (c) 2009-2011 Code Synthesis Tools CC
+// license   : GNU GPL v2; see accompanying LICENSE file
+
+#ifndef TEST_HXX
+#define TEST_HXX
+
+#include <vector>
+#include <cstring> // std::memcmp
+
+#include <odb/core.hxx>
+
+#ifdef ODB_COMPILER
+#  ifdef ODB_DATABASE_PGSQL
+#    define BLOB_TYPE "BYTEA"
+#  else
+#    define BLOB_TYPE "BLOB"
+#  endif
+#endif
+
+#pragma db object
+struct object
+{
+  object () {}
+  object (unsigned long id): id_ (id) {}
+
+  #pragma db id
+  unsigned long id_;
+
+  #pragma db type(BLOB_TYPE)
+  std::vector<char> vc;
+
+  #pragma db type(BLOB_TYPE)
+  std::vector<unsigned char> vuc;
+
+  #pragma db type(BLOB_TYPE)
+  char c[1024];
+
+  #pragma db type(BLOB_TYPE)
+  unsigned char uc[1024];
+};
+
+inline bool
+operator== (const object& x, const object& y)
+{
+  return
+    x.id_ == y.id_ &&
+    x.vc == y.vc &&
+    x.vuc == y.vuc &&
+    std::memcmp (x.c, y.c, sizeof (x.c)) == 0 &&
+    std::memcmp (x.uc, y.uc, sizeof (x.uc)) == 0;
+}
+
+#endif // TEST_HXX
