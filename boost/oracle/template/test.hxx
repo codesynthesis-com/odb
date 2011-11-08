20c2f6cde170e1a8703858e17530fcf68e4efbe4
Constantin Michael <constantin@codesynthesis.com>
2011-11-08 13:42:45 +0200

    Add tests for Oracle support of the Boost profile

---
+++ boost/oracle/template/test.hxx
@@ -0,0 +1,27 @@
+// file      : boost/oracle/template/test.hxx
+// author    : Boris Kolpackov <boris@codesynthesis.com>
+// copyright : Copyright (c) 2009-2011 Code Synthesis Tools CC
+// license   : GNU GPL v2; see accompanying LICENSE file
+
+#ifndef TEST_HXX
+#define TEST_HXX
+
+#include <odb/core.hxx>
+
+#pragma db object
+struct object
+{
+  object (unsigned long id)
+      : id_ (id)
+  {
+  }
+
+  object ()
+  {
+  }
+
+  #pragma db id
+  unsigned long id_;
+};
+
+#endif // TEST_HXX
