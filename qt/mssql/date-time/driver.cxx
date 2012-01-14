0da5345276f1e655e5e099fdf6280466e3a5dda4
Boris Kolpackov <boris@codesynthesis.com>
2012-01-14 09:43:38 +0200

    Date-time part of Qt profile implementation for SQL Server

---
+++ qt/mssql/date-time/driver.cxx
@@ -0,0 +1,92 @@
+// file      : qt/mssql/date-time/driver.cxx
+// author    : Constantin Michael <constantin@codesynthesis.com>
+// copyright : Copyright (c) 2009-2011 Code Synthesis Tools CC
+// license   : GNU GPL v2; see accompanying LICENSE file
+
+// Test Qt date/time type persistence. SQL Server version.
+//
+
+#include <memory>   // std::auto_ptr
+#include <cassert>
+#include <iostream>
+
+#include <QtCore/QDateTime>
+#include <QtCore/QCoreApplication>
+
+#include <odb/mssql/database.hxx>
+#include <odb/mssql/transaction.hxx>
+
+#include <common/common.hxx>
+
+#include "test.hxx"
+#include "test-odb.hxx"
+
+using namespace std;
+using namespace odb::core;
+
+int
+main (int argc, char* argv[])
+{
+  QCoreApplication app (argc, argv);
+
+  try
+  {
+    auto_ptr<database> db (create_database (argc, argv));
+
+    object o;
+
+    // Check persistence of null values.
+    //
+    {
+      transaction t (db->begin ());
+      db->persist (o);
+      t.commit ();
+    }
+
+    {
+      transaction t (db->begin ());
+      auto_ptr<object> ol (db->load<object> (o.id));
+      t.commit ();
+
+      assert (ol->is_null ());
+    }
+
+    // Check persistence of valid dates and times.
+    //
+    QDateTime t (QDateTime::currentDateTime ());
+
+    o.date = t.date ();
+    o.date_time = t;
+
+    // In DATETIME fractional seconds are rounded to .000, .003, or .007.
+    //
+    o.date_time_dt = QDateTime (QDate (2012, 1, 14),
+                                QTime (11, 57, 13, 3));
+
+    // SMALLDATETIME doesn't have seconds (always 0).
+    //
+    o.date_time_sdt = QDateTime (QDate (2012, 1, 14),
+                                 QTime (11, 57, 0, 0));
+
+    o.time = t.time ();
+
+    {
+      transaction t (db->begin ());
+      db->persist (o);
+      t.commit ();
+    }
+
+    {
+      transaction t (db->begin ());
+      auto_ptr<object> ol (db->load<object> (o.id));
+      t.commit ();
+
+      assert (*ol == o);
+    }
+  }
+  catch (const odb::exception& e)
+  {
+    cerr << e.what () << endl;
+    return 1;
+  }
+}
---
+++ qt/mssql/template/driver.cxx
@@ -0,0 +1,48 @@
+// file      : qt/mssql/template/driver.cxx
+// author    : Constantin Michael <constantin@codesynthesis.com>
+// copyright : Copyright (c) 2009-2011 Code Synthesis Tools CC
+// license   : GNU GPL v2; see accompanying LICENSE file
+
+// PLACE TEST DESCRIPTION HERE
+//
+
+#include <memory>   // std::auto_ptr
+#include <cassert>
+#include <iostream>
+
+#include <QtCore/QCoreApplication>
+
+#include <odb/mssql/database.hxx>
+#include <odb/mssql/transaction.hxx>
+
+#include <common/common.hxx>
+
+#include "test.hxx"
+#include "test-odb.hxx"
+
+using namespace std;
+using namespace odb::core;
+
+int
+main (int argc, char* argv[])
+{
+  QCoreApplication app (argc, argv);
+
+  try
+  {
+    auto_ptr<database> db (create_database (argc, argv));
+
+    //
+    //
+    cout << "test 001" << endl;
+    {
+      transaction t (db->begin ());
+      t.commit ();
+    }
+  }
+  catch (const odb::exception& e)
+  {
+    cerr << e.what () << endl;
+    return 1;
+  }
+}
