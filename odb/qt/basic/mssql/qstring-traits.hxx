94ad197a8b1dc7faeacd69cd269a9f80681036cd
Boris Kolpackov <boris@codesynthesis.com>
2012-01-29 14:25:56 +0200

    Remove author field from file header
    Too much effort to maintain.

--- odb/qt/basic/mssql/qstring-traits.hxx
+++ odb/qt/basic/mssql/qstring-traits.hxx
@@ -1,5 +1,4 @@
 // file      : odb/qt/basic/mssql/qstring-traits.hxx
-// author    : Constantin Michael <constantin@codesynthesis.com>
 // copyright : Copyright (c) 2009-2011 Code Synthesis Tools CC
 // license   : GNU GPL v2; see accompanying LICENSE file
 
