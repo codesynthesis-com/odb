d9fc08b05e98410bce05d9f359518b1a7fa6fd10
Boris Kolpackov <boris@codesynthesis.com>
2012-09-14 08:29:54 +0200

    Bump version to 2.1.0

--- odb/oracle/version.hxx
+++ odb/oracle/version.hxx
@@ -29,15 +29,15 @@
 
 // Check that we have compatible ODB version.
 //
-#if ODB_VERSION != 20002
+#if ODB_VERSION != 20100
 #  error incompatible odb interface version detected
 #endif
 
 // libodb-oracle version: odb interface version plus the bugfix
 // version.
 //
-#define LIBODB_ORACLE_VERSION     2009902
-#define LIBODB_ORACLE_VERSION_STR "2.1.0.a2"
+#define LIBODB_ORACLE_VERSION     2010000
+#define LIBODB_ORACLE_VERSION_STR "2.1.0"
 
 #include <odb/post.hxx>
 
