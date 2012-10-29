83374c27062772de95ab12652713bfb2c218ba48
Boris Kolpackov <boris@codesynthesis.com>
2012-10-29 16:33:00 +0200

    Don't use boolean as identifier
    Some headers/systems defined it as a macro.

--- odb/oracle/query.cxx
+++ odb/oracle/query.cxx
@@ -88,7 +88,8 @@ namespace odb
     void query_base::
     append (const string& q)
     {
-      if (!clause_.empty () && clause_.back ().kind == clause_part::native)
+      if (!clause_.empty () &&
+          clause_.back ().kind == clause_part::kind_native)
       {
         string& s (clause_.back ().part);
 
@@ -105,7 +106,7 @@ namespace odb
         s += q;
       }
       else
-        clause_.push_back (clause_part (clause_part::native, q));
+        clause_.push_back (clause_part (clause_part::kind_native, q));
     }
 
     void query_base::
@@ -115,13 +116,13 @@ namespace odb
       s += '.';
       s += column;
 
-      clause_.push_back (clause_part (clause_part::column, s));
+      clause_.push_back (clause_part (clause_part::kind_column, s));
     }
 
     void query_base::
     add (details::shared_ptr<query_param> p, const char* conv)
     {
-      clause_.push_back (clause_part (clause_part::param));
+      clause_.push_back (clause_part (clause_part::kind_param));
 
       if (conv != 0)
         clause_.back ().part = conv;
@@ -200,12 +201,12 @@ namespace odb
       //
       clause_type::iterator i (clause_.begin ()), e (clause_.end ());
 
-      if (i != e && i->kind == clause_part::boolean && i->bool_part)
+      if (i != e && i->kind == clause_part::kind_bool && i->bool_part)
       {
         clause_type::iterator j (i + 1);
 
         if (j == e ||
-            (j->kind == clause_part::native && check_prefix (j->part)))
+            (j->kind == clause_part::kind_native && check_prefix (j->part)))
           clause_.erase (i);
       }
     }
@@ -217,7 +218,7 @@ namespace odb
       {
         const clause_part& p (clause_.front ());
 
-        if (p.kind == clause_part::native && check_prefix (p.part))
+        if (p.kind == clause_part::kind_native && check_prefix (p.part))
           return "";
 
         return "WHERE ";
@@ -241,7 +242,7 @@ namespace odb
 
         switch (i->kind)
         {
-        case clause_part::column:
+        case clause_part::kind_column:
           {
             if (last != ' ' && last != '(')
               r += ' ';
@@ -249,7 +250,7 @@ namespace odb
             r += i->part;
             break;
           }
-        case clause_part::param:
+        case clause_part::kind_param:
           {
             if (last != ' ' && last != '(')
               r += ' ';
@@ -274,7 +275,7 @@ namespace odb
 
             break;
           }
-        case clause_part::native:
+        case clause_part::kind_native:
           {
             // We don't want extra spaces after '(' as well as before ','
             // and ')'.
@@ -289,7 +290,7 @@ namespace odb
             r += p;
             break;
           }
-        case clause_part::boolean:
+        case clause_part::kind_bool:
           {
             if (last != ' ' && last != '(')
               r += ' ';
