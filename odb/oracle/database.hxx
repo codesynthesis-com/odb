43dc08e8df03eac590f92c4d92ecdb14c72050e2
Boris Kolpackov <boris@codesynthesis.com>
2013-01-17 10:55:13 +0200

    Add database id constant (useful for meta-programming)

--- odb/oracle/database.hxx
+++ odb/oracle/database.hxx
@@ -379,6 +379,11 @@ namespace odb
       using odb::database::tracer;
 
     public:
+      // Database id constant (useful for meta-programming).
+      //
+      static const odb::database_id database_id = id_oracle;
+
+    public:
       virtual
       ~database ();
 
