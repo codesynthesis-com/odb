0c5c65f4377db0d6e303dec900e5561f6afdf7d3
Boris Kolpackov <boris@codesynthesis.com>
2013-02-07 08:27:09 +0200

    Take care of QT_REDUCE_RELOCATIONS-related error for ODB compiler

--- odb/qt/details/config.hxx
+++ odb/qt/details/config.hxx
@@ -9,6 +9,23 @@
 
 #ifdef _MSC_VER
 #elif defined(ODB_COMPILER)
+// Qt5 may complain if we are building without -fPIC or -fPIE. Instead of
+// asking the user to pass one of these options to the ODB compiler (which
+// can, BTW, be done with -x -fPIE, for example, if one is not using the
+// Qt profile), we are going to define __PIE__ ourselves just to silence
+// Qt. We also want to try to minimize this to cases where it is actually
+// necessary. To achieve this, we need to include the Qt config file without
+// including <QtGlobals>, which is where the test for PIC/PIE is. While
+// newer versions of Qt (from 4.7) have <QtConfig>, to support older
+// versions we will include qconfig.h directly. This file appears to be
+// present in all the versions starting with Qt 4.0.
+//
+#  if defined(__ELF__) && !defined(__PIC__) && !defined(__PIE__)
+#    include <QtCore/qconfig.h> // QT_REDUCE_RELOCATIONS
+#    ifdef QT_REDUCE_RELOCATIONS
+#      define __PIE__
+#    endif
+#  endif
 #  define LIBODB_QT_STATIC_LIB
 #else
 #  include <odb/qt/details/config.h>
