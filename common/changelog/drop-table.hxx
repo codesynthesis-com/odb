15c6024da6a15e2d6e3302e4df375092959edbe2
Boris Kolpackov <boris@codesynthesis.com>
2013-03-22 13:55:18 +0200

    Add changelog test

---
+++ common/changelog/drop-table.hxx
@@ -0,0 +1,35 @@
+// file      : common/changelog/drop-table.hxx
+// copyright : Copyright (c) 2009-2013 Code Synthesis Tools CC
+// license   : GNU GPL v2; see accompanying LICENSE file
+
+#ifndef DROP_TABLE_HXX
+#define DROP_TABLE_HXX
+
+#include <vector>
+
+#pragma db model version(BVER, CVER, true)
+
+struct object1;
+
+#if CVER == 1
+#pragma db object
+struct object
+{
+  #pragma db id auto
+  int id;
+  int num;
+
+  std::vector<int> nums;
+  object1* o1;
+};
+#endif
+
+#pragma db object
+struct object1
+{
+  #pragma db id
+  int id;
+  int num;
+};
+
+#endif // DROP_TABLE_HXX
