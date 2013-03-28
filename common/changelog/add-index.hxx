e7f6315882ef16939f99dd757eaf800b9f12e92b
Boris Kolpackov <boris@codesynthesis.com>
2013-03-28 16:04:48 +0200

    Add changelog support for add/drop index/foreign key
    Also diagnose changes to primary keys and establish the 'alters'
    association.

---
+++ common/changelog/add-index.hxx
@@ -0,0 +1,25 @@
+// file      : common/changelog/add-index.hxx
+// copyright : Copyright (c) 2009-2013 Code Synthesis Tools CC
+// license   : GNU GPL v2; see accompanying LICENSE file
+
+#ifndef ADD_INDEX_HXX
+#define ADD_INDEX_HXX
+
+#pragma db model version(BVER, CVER, true)
+
+#pragma db object
+struct object
+{
+  #pragma db id auto
+  int id;
+
+  int x;
+
+#if CVER > 1
+  int y;
+  #pragma db index ("xy_i") unique member(x) member(y, "DESC")
+#endif
+
+};
+
+#endif // ADD_INDEX_HXX
---
+++ common/changelog/drop-index.hxx
@@ -0,0 +1,22 @@
+// file      : common/changelog/drop-index.hxx
+// copyright : Copyright (c) 2009-2013 Code Synthesis Tools CC
+// license   : GNU GPL v2; see accompanying LICENSE file
+
+#ifndef DROP_INDEX_HXX
+#define DROP_INDEX_HXX
+
+#pragma db model version(BVER, CVER, true)
+
+#pragma db object
+struct object
+{
+  #pragma db id auto
+  int id;
+
+#if CVER == 1
+  #pragma db index
+#endif
+  int num;
+};
+
+#endif // DROP_INDEX_HXX
