31b400b73be9aa98c0fd448423677db212667c43
Boris Kolpackov <boris@codesynthesis.com>
2013-04-04 09:47:33 +0200

    Generate add/drop index migration statements

---
+++ evolution/add-index/test1.hxx
@@ -0,0 +1,12 @@
+// file      : evolution/add-index/test1.hxx
+// copyright : Copyright (c) 2009-2013 Code Synthesis Tools CC
+// license   : GNU GPL v2; see accompanying LICENSE file
+
+#ifndef TEST1_HXX
+#define TEST1_HXX
+
+#define MODEL_VERSION 2
+#include "model.hxx"
+#undef MODEL_VERSION
+
+#endif // TEST1_HXX
