c34d4bf4617beb0b7f7b0ad370a33c707d660dce
Boris Kolpackov <boris@codesynthesis.com>
2013-12-12 10:23:26 +0200

    Bump version to 2.4.0.a1

--- odb/pgsql/version.hxx
+++ odb/pgsql/version.hxx
@@ -29,15 +29,15 @@
 
 // Check that we have compatible ODB version.
 //
-#if ODB_VERSION != 20300
+#if ODB_VERSION != 20301
 #  error incompatible odb interface version detected
 #endif
 
 // libodb-pgsql version: odb interface version plus the bugfix
 // version.
 //
-#define LIBODB_PGSQL_VERSION     2030000
-#define LIBODB_PGSQL_VERSION_STR "2.3.0"
+#define LIBODB_PGSQL_VERSION     2039901
+#define LIBODB_PGSQL_VERSION_STR "2.4.0.a1"
 
 #include <odb/post.hxx>
 
