67ab05aa4ce916bf2b170d5881af7291f691afbb
Boris Kolpackov <boris@codesynthesis.com>
2014-01-18 10:33:57 +0200

    Bump version to 2.3.1

--- odb/version.hxx
+++ odb/version.hxx
@@ -29,7 +29,7 @@
 
 // ODB compiler version: interface version plus the bugfix version.
 //
-#define ODB_COMPILER_VERSION     2030000
-#define ODB_COMPILER_VERSION_STR "2.3.0"
+#define ODB_COMPILER_VERSION     2030100
+#define ODB_COMPILER_VERSION_STR "2.3.1"
 
 #endif // ODB_VERSION_HXX
