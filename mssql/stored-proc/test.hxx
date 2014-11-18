092cb7d916b8ebc83cacab849010acd04f56d38d
Boris Kolpackov <boris@codesynthesis.com>
2014-11-18 14:54:23 +0200

    Add SQL Server stored procedure test

---
+++ mssql/stored-proc/test.hxx
@@ -0,0 +1,58 @@
+// file      : mssql/stored-proc/test.hxx
+// copyright : Copyright (c) 2009-2013 Code Synthesis Tools CC
+// license   : GNU GPL v2; see accompanying LICENSE file
+
+#ifndef TEST_HXX
+#define TEST_HXX
+
+#include <string>
+
+#include <odb/core.hxx>
+
+#pragma db view query("SELECT SCHEMA_NAME()")
+struct default_schema
+{
+  std::string name;
+};
+
+#pragma db object
+struct object
+{
+  object () {}
+  object (unsigned int n, std::string s): num (n), str (s) {}
+
+  #pragma db id auto
+  unsigned long id;
+
+  unsigned int num;
+  std::string str;
+};
+
+#pragma db view query("EXEC select_all_objects")
+struct select_all_objects
+{
+  unsigned int num;
+  std::string str;
+};
+
+#pragma db view query("EXEC select_objects (?)")
+struct select_objects
+{
+  std::string str;
+};
+
+#pragma db view query("EXEC objects_min_max_odb (?)")
+struct objects_min_max
+{
+  unsigned int num_min;
+  unsigned int num_max;
+};
+
+#pragma db view query("EXEC insert_object_odb (?)")
+struct insert_object
+{
+  unsigned int ret;
+  unsigned long id;
+};
+
+#endif // TEST_HXX
