a130d40763b6237af1c7424bbb291f82e40aed70
Boris Kolpackov <boris@codesynthesis.com>
2015-02-06 09:31:17 +0200

    Update copyright

--- odb/pgsql/pgsql-oid.hxx
+++ odb/pgsql/pgsql-oid.hxx
@@ -1,5 +1,5 @@
 // file      : odb/pgsql/pgsql-oid.hxx
-// copyright : Copyright (c) 2005-2013 Code Synthesis Tools CC
+// copyright : Copyright (c) 2005-2015 Code Synthesis Tools CC
 // license   : GNU GPL v2; see accompanying LICENSE file
 
 // Generated file of OIDs extracted from the PostgreSQL 8.4.8 source file
