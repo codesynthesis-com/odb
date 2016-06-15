b431626960d64101300d0861258eb7aa30cba6db
Boris Kolpackov <boris@codesynthesis.com>
2016-06-15 18:56:48 +0200

    Bump version to 2.5.0.a8

--- odb/mssql/version.hxx
+++ odb/mssql/version.hxx
@@ -29,15 +29,15 @@
 
 // Check that we have compatible ODB version.
 //
-#if ODB_VERSION != 20407
+#if ODB_VERSION != 20408
 #  error incompatible odb interface version detected
 #endif
 
 // libodb-mssql version: odb interface version plus the bugfix
 // version.
 //
-#define LIBODB_MSSQL_VERSION     2049907
-#define LIBODB_MSSQL_VERSION_STR "2.5.0.a7"
+#define LIBODB_MSSQL_VERSION     2049908
+#define LIBODB_MSSQL_VERSION_STR "2.5.0.a8"
 
 #include <odb/post.hxx>
 
