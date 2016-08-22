a2cfb9269beaedfe0617ef7b679d13edb519c460
Boris Kolpackov <boris@codesynthesis.com>
2016-08-22 12:59:41 +0200

    Bump version to 2.5.0-a10

--- odb/qt/version.hxx
+++ odb/qt/version.hxx
@@ -28,23 +28,23 @@
 
 // Check that we have compatible ODB version.
 //
-#if ODB_VERSION != 20409
+#if ODB_VERSION != 20410
 #  error incompatible odb interface version detected
 #endif
 
 // ODB Qt interface version: odb interface version plus the Qt interface
 // version.
 //
-#define ODB_QT_VERSION     2040900
-#define ODB_QT_VERSION_STR "2.5.0-a9"
+#define ODB_QT_VERSION     2041000
+#define ODB_QT_VERSION_STR "2.5.0-a10"
 
 // libodb-qt version: odb interface version plus the bugfix version. Note
 // that LIBODB_QT_VERSION is always greater or equal to ODB_QT_VERSION
 // since if the Qt interface virsion is incremented then the bugfix version
 // must be incremented as well.
 //
-#define LIBODB_QT_VERSION     2049909
-#define LIBODB_QT_VERSION_STR "2.5.0-a9"
+#define LIBODB_QT_VERSION     2049910
+#define LIBODB_QT_VERSION_STR "2.5.0-a10"
 
 #include <odb/post.hxx>
 
