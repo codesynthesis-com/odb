c11a0e411d62db6f2ad4153579af254081a0e6f0
Boris Kolpackov <boris@codesynthesis.com>
2016-10-26 16:41:51 +0200

    Implement support for mixed auto/manual id assignment in SQLite
    Now one can do:
    
    #pragma db id auto
    odb::nullable<int64_t> id;
    
    And then set the id to NULL to get auto-assignment or to the actual value
    to use a manual id.

---
+++ sqlite/auto/driver.cxx
@@ -0,0 +1,75 @@
+// file      : sqlite/auto/driver.cxx
+// copyright : Copyright (c) 2009-2015 Code Synthesis Tools CC
+// license   : GNU GPL v2; see accompanying LICENSE file
+
+// Test manual/automatic id assignment.
+//
+
+#include <memory>   // std::auto_ptr
+#include <cassert>
+#include <iostream>
+
+#include <odb/sqlite/database.hxx>
+#include <odb/sqlite/transaction.hxx>
+
+#include <common/common.hxx>
+
+#include "test.hxx"
+#include "test-odb.hxx"
+
+using namespace std;
+namespace sqlite = odb::sqlite;
+using namespace sqlite;
+
+int
+main (int argc, char* argv[])
+{
+  try
+  {
+    auto_ptr<database> db (create_specific_database<database> (argc, argv));
+
+    // object
+    //
+    {
+      unsigned long id1, id2, id3;
+      {
+        object o1 (1, "one");
+        object o2 ("two");
+        object o3 (3, "three");
+
+        transaction t (db->begin ());
+        db->persist (o1);
+        db->persist (o2);
+        db->persist (o3);
+        t.commit ();
+
+        id1 = *o1.id_;
+        id2 = *o2.id_;
+        id3 = *o3.id_;
+
+        assert (id1 == 1);
+        assert (id3 == 3);
+
+        assert (id2 != id1);
+        assert (id2 != id3);
+      }
+
+      {
+        transaction t (db->begin ());
+        auto_ptr<object> o1 (db->load<object> (id1));
+        auto_ptr<object> o2 (db->load<object> (id2));
+        auto_ptr<object> o3 (db->load<object> (id3));
+        t.commit ();
+
+        assert (*o1->id_ == id1 && o1->str_ == "one");
+        assert (*o2->id_ == id2 && o2->str_ == "two");
+        assert (*o3->id_ == id3 && o3->str_ == "three");
+      }
+    }
+  }
+  catch (const odb::exception& e)
+  {
+    cerr << e.what () << endl;
+    return 1;
+  }
+}
---
+++ sqlite/auto/test.hxx
@@ -0,0 +1,33 @@
+// file      : sqlite/auto/test.hxx
+// copyright : Copyright (c) 2009-2015 Code Synthesis Tools CC
+// license   : GNU GPL v2; see accompanying LICENSE file
+
+#ifndef TEST_HXX
+#define TEST_HXX
+
+#include <odb/core.hxx>
+#include <odb/nullable.hxx>
+
+#include <string>
+
+#include <odb/core.hxx>
+
+#pragma db object
+struct object
+{
+  explicit
+  object (const std::string& str): str_ (str) {}
+  object (unsigned long id, const std::string& str): id_ (id), str_ (str) {}
+
+  #pragma db auto id
+  odb::nullable<unsigned long> id_;
+
+  std::string str_;
+
+private:
+  object () {}
+
+  friend class odb::access;
+};
+
+#endif // TEST_HXX
