ebe3b3b99916a0b7ff04c8150d65feb4ea32e3c9
Boris Kolpackov <boris@codesynthesis.com>
2017-01-03 14:17:35 +0200

    Update copyright year

--- odb/qt/basic/oracle/default-mapping.hxx
+++ odb/qt/basic/oracle/default-mapping.hxx
@@ -1,5 +1,5 @@
 // file      : odb/qt/basic/oracle/default-mapping.hxx
-// copyright : Copyright (c) 2009-2015 Code Synthesis Tools CC
+// copyright : Copyright (c) 2009-2017 Code Synthesis Tools CC
 // license   : GNU GPL v2; see accompanying LICENSE file
 
 #ifndef ODB_QT_BASIC_ORACLE_DEFAULT_MAPPING_HXX
--- odb/qt/smart-ptr/pointer-traits.hxx
+++ odb/qt/smart-ptr/pointer-traits.hxx
@@ -1,5 +1,5 @@
 // file      : odb/qt/smart-ptr/pointer-traits.hxx
-// copyright : Copyright (c) 2009-2015 Code Synthesis Tools CC
+// copyright : Copyright (c) 2009-2017 Code Synthesis Tools CC
 // license   : GNU GPL v2; see accompanying LICENSE file
 
 #ifndef ODB_QT_SMART_PTR_POINTER_TRAITS_HXX
