300e19c6c389d1126dc77688dba30b8bc2811c88
Boris Kolpackov <boris@codesynthesis.com>
2017-08-08 14:08:25 +0200

    Bump version to 2.5.0-b.6.z, master is open for business

--- odb/boost/version.hxx
+++ odb/boost/version.hxx
@@ -28,23 +28,23 @@
 
 // Check that we have compatible ODB version.
 //
-#if ODB_VERSION != 20455
+#if ODB_VERSION != 20456
 #  error incompatible odb interface version detected
 #endif
 
 // ODB Boost interface version: odb interface version plus the Boost interface
 // version.
 //
-#define ODB_BOOST_VERSION     2045500
-#define ODB_BOOST_VERSION_STR "2.5.0-b.5"
+#define ODB_BOOST_VERSION     2045600
+#define ODB_BOOST_VERSION_STR "2.5.0-b.6"
 
 // libodb-boost version: odb interface version plus the bugfix version. Note
 // that LIBODB_BOOST_VERSION is always greater or equal to ODB_BOOST_VERSION
 // since if the Boost interface virsion is incremented then the bugfix version
 // must be incremented as well.
 //
-#define LIBODB_BOOST_VERSION     2049955
-#define LIBODB_BOOST_VERSION_STR "2.5.0-b.5"
+#define LIBODB_BOOST_VERSION     2049956
+#define LIBODB_BOOST_VERSION_STR "2.5.0-b.6"
 
 #include <odb/post.hxx>
 
