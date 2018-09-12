89896fb1f7f3e627b756cdf183b3e7197e059777
Boris Kolpackov <boris@codesynthesis.com>
2018-09-12 14:26:01 +0200

    Bump version to 2.5.0-b.10.z, master is open for business

--- odb/mssql/version.hxx
+++ odb/mssql/version.hxx
@@ -33,15 +33,15 @@
 
 // Check that we have compatible ODB version.
 //
-#if ODB_VERSION != 20459
+#if ODB_VERSION != 20460
 #  error incompatible odb interface version detected
 #endif
 
 // libodb-mssql version: odb interface version plus the bugfix
 // version.
 //
-#define LIBODB_MSSQL_VERSION     2049959
-#define LIBODB_MSSQL_VERSION_STR "2.5.0-b.9"
+#define LIBODB_MSSQL_VERSION     2049960
+#define LIBODB_MSSQL_VERSION_STR "2.5.0-b.10"
 
 #include <odb/post.hxx>
 
