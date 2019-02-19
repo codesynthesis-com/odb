678c1cf37394b81e727efe2c2f87a019bc5405de
Boris Kolpackov <boris@codesynthesis.com>
2019-02-19 07:44:57 +0200

    Release version 2.5.0-b.11

--- odb/pgsql/version.hxx
+++ odb/pgsql/version.hxx
@@ -33,15 +33,15 @@
 
 // Check that we have compatible ODB version.
 //
-#if ODB_VERSION != 20460
+#if ODB_VERSION != 20461
 #  error incompatible odb interface version detected
 #endif
 
 // libodb-pgsql version: odb interface version plus the bugfix
 // version.
 //
-#define LIBODB_PGSQL_VERSION     2049960
-#define LIBODB_PGSQL_VERSION_STR "2.5.0-b.10"
+#define LIBODB_PGSQL_VERSION     2049961
+#define LIBODB_PGSQL_VERSION_STR "2.5.0-b.11"
 
 #include <odb/post.hxx>
 
