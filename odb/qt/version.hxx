19c68c0b7c622dd26cc31248d66cc78f7a61dc0f
Boris Kolpackov <boris@codesynthesis.com>
2019-02-19 07:44:57 +0200

    Release version 2.5.0-b.11

--- odb/qt/version.hxx
+++ odb/qt/version.hxx
@@ -7,8 +7,8 @@
 
 // @@ TODO: need to derive automatically (it is also hardcoded in *.options).
 //
-#define ODB_QT_VERSION     2046000
-#define ODB_QT_VERSION_STR "2.5.0-b.10"
+#define ODB_QT_VERSION     2046100
+#define ODB_QT_VERSION_STR "2.5.0-b.11"
 
 #else
 
@@ -38,23 +38,23 @@
 
 // Check that we have compatible ODB version.
 //
-#if ODB_VERSION != 20460
+#if ODB_VERSION != 20461
 #  error incompatible odb interface version detected
 #endif
 
 // ODB Qt interface version: odb interface version plus the Qt interface
 // version.
 //
-#define ODB_QT_VERSION     2046000
-#define ODB_QT_VERSION_STR "2.5.0-b.10"
+#define ODB_QT_VERSION     2046100
+#define ODB_QT_VERSION_STR "2.5.0-b.11"
 
 // libodb-qt version: odb interface version plus the bugfix version. Note
 // that LIBODB_QT_VERSION is always greater or equal to ODB_QT_VERSION
 // since if the Qt interface virsion is incremented then the bugfix version
 // must be incremented as well.
 //
-#define LIBODB_QT_VERSION     2049960
-#define LIBODB_QT_VERSION_STR "2.5.0-b.10"
+#define LIBODB_QT_VERSION     2049961
+#define LIBODB_QT_VERSION_STR "2.5.0-b.11"
 
 #include <odb/post.hxx>
 
