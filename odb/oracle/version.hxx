03d2fccf403475db0cc2d8295f8714b80a92b61b
Boris Kolpackov <boris@codesynthesis.com>
2019-03-20 17:13:50 +0200

    Change version to 2.5.0-b.14.z

--- odb/oracle/version.hxx
+++ odb/oracle/version.hxx
@@ -33,15 +33,15 @@
 
 // Check that we have compatible ODB version.
 //
-#if ODB_VERSION != 20463
+#if ODB_VERSION != 20464
 #  error incompatible odb interface version detected
 #endif
 
 // libodb-oracle version: odb interface version plus the bugfix
 // version.
 //
-#define LIBODB_ORACLE_VERSION     2049963
-#define LIBODB_ORACLE_VERSION_STR "2.5.0-b.13"
+#define LIBODB_ORACLE_VERSION     2049964
+#define LIBODB_ORACLE_VERSION_STR "2.5.0-b.14"
 
 #include <odb/post.hxx>
 
