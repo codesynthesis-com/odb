747c837824ded17822113a83b785b0bee757acdd
Boris Kolpackov <boris@codesynthesis.com>
2019-03-20 17:13:50 +0200

    Change version to 2.5.0-b.14.z

--- odb/qt/version.hxx
+++ odb/qt/version.hxx
@@ -32,23 +32,23 @@
 
 // Check that we have compatible ODB version.
 //
-#if ODB_VERSION != 20463
+#if ODB_VERSION != 20464
 #  error incompatible odb interface version detected
 #endif
 
 // ODB Qt interface version: odb interface version plus the Qt interface
 // version.
 //
-#define ODB_QT_VERSION     2046300
-#define ODB_QT_VERSION_STR "2.5.0-b.13"
+#define ODB_QT_VERSION     2046400
+#define ODB_QT_VERSION_STR "2.5.0-b.14"
 
 // libodb-qt version: odb interface version plus the bugfix version. Note
 // that LIBODB_QT_VERSION is always greater or equal to ODB_QT_VERSION
 // since if the Qt interface virsion is incremented then the bugfix version
 // must be incremented as well.
 //
-#define LIBODB_QT_VERSION     2049963
-#define LIBODB_QT_VERSION_STR "2.5.0-b.13"
+#define LIBODB_QT_VERSION     2049964
+#define LIBODB_QT_VERSION_STR "2.5.0-b.14"
 
 #include <odb/post.hxx>
 
