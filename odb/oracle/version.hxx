0c7d3208d488368bf7644b25b9c919f367502f05
Boris Kolpackov <boris@codesynthesis.com>
2020-07-21 15:13:40 +0200

    Change version to 2.5.0-b.20.z

--- odb/oracle/version.hxx
+++ odb/oracle/version.hxx
@@ -32,15 +32,15 @@
 
 // Check that we have compatible ODB version.
 //
-#if ODB_VERSION != 20469
+#if ODB_VERSION != 20470
 #  error incompatible odb interface version detected
 #endif
 
 // libodb-oracle version: odb interface version plus the bugfix
 // version.
 //
-#define LIBODB_ORACLE_VERSION     2049969
-#define LIBODB_ORACLE_VERSION_STR "2.5.0-b.19"
+#define LIBODB_ORACLE_VERSION     2049970
+#define LIBODB_ORACLE_VERSION_STR "2.5.0-b.20"
 
 #include <odb/post.hxx>
 
