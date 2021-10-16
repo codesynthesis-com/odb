be7daa6dbb1a7519c1d493983ba85b7084ecb135
Boris Kolpackov <boris@codesynthesis.com>
2021-10-16 11:12:52 +0200

    Release version 2.5.0-b.21

--- odb/oracle/version.hxx
+++ odb/oracle/version.hxx
@@ -32,15 +32,15 @@
 
 // Check that we have compatible ODB version.
 //
-#if ODB_VERSION != 20470
+#if ODB_VERSION != 20471
 #  error incompatible odb interface version detected
 #endif
 
 // libodb-oracle version: odb interface version plus the bugfix
 // version.
 //
-#define LIBODB_ORACLE_VERSION     2049970
-#define LIBODB_ORACLE_VERSION_STR "2.5.0-b.20"
+#define LIBODB_ORACLE_VERSION     2049971
+#define LIBODB_ORACLE_VERSION_STR "2.5.0-b.21"
 
 #include <odb/post.hxx>
 
