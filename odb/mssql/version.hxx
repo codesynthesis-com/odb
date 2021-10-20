ee88b16be5300c228ee4d11e9e1189258275e9c0
Boris Kolpackov <boris@codesynthesis.com>
2021-10-20 09:01:10 +0200

    Change version to 2.5.0-b.22.z

--- odb/mssql/version.hxx
+++ odb/mssql/version.hxx
@@ -32,15 +32,15 @@
 
 // Check that we have compatible ODB version.
 //
-#if ODB_VERSION != 20471
+#if ODB_VERSION != 20472
 #  error incompatible odb interface version detected
 #endif
 
 // libodb-mssql version: odb interface version plus the bugfix
 // version.
 //
-#define LIBODB_MSSQL_VERSION     2049971
-#define LIBODB_MSSQL_VERSION_STR "2.5.0-b.21"
+#define LIBODB_MSSQL_VERSION     2049972
+#define LIBODB_MSSQL_VERSION_STR "2.5.0-b.22"
 
 #include <odb/post.hxx>
 
