dbfa1d1e5b5b89a4828039ad9d252672d673ff2d
Boris Kolpackov <boris@codesynthesis.com>
2023-06-30 13:05:40 +0200

    Release version 2.5.0-b.25

--- odb/qt/version.hxx
+++ odb/qt/version.hxx
@@ -31,23 +31,23 @@
 
 // Check that we have compatible ODB version.
 //
-#if ODB_VERSION != 20474
+#if ODB_VERSION != 20475
 #  error incompatible odb interface version detected
 #endif
 
 // ODB Qt interface version: odb interface version plus the Qt interface
 // version.
 //
-#define ODB_QT_VERSION     2047400
-#define ODB_QT_VERSION_STR "2.5.0-b.24"
+#define ODB_QT_VERSION     2047500
+#define ODB_QT_VERSION_STR "2.5.0-b.25"
 
 // libodb-qt version: odb interface version plus the bugfix version. Note
 // that LIBODB_QT_VERSION is always greater or equal to ODB_QT_VERSION
 // since if the Qt interface virsion is incremented then the bugfix version
 // must be incremented as well.
 //
-#define LIBODB_QT_VERSION     2049974
-#define LIBODB_QT_VERSION_STR "2.5.0-b.24"
+#define LIBODB_QT_VERSION     2049975
+#define LIBODB_QT_VERSION_STR "2.5.0-b.25"
 
 #include <odb/post.hxx>
 
